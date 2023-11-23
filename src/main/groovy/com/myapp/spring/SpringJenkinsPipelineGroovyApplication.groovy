15f5b502c7be033dee15b4bce43cb10f809ee973
manjushreebugowda <150430697+manjushreebugowda@users.noreply.github.com>
2023-11-23 13:12:09 +0530

    Update SpringJenkinsPipelineGroovyApplication.groovy

--- src/main/groovy/com/myapp/spring/SpringJenkinsPipelineGroovyApplication.groovy
+++ src/main/groovy/com/myapp/spring/SpringJenkinsPipelineGroovyApplication.groovy
@@ -1,5 +1,5 @@
 package com.myapp.spring
-
+//changes done by me
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 // changes done
